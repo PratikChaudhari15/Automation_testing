cad7c1a547dfacd628dcee7ca18f645ecbdeccca
Pratik Chaudhari <pratik.chaudhari15.03@gmail.com>
2025-09-30 15:48:42 +0530

    Locator Code Updated

---
+++ src/Basics/Direct_Locator29_09.java
@@ -0,0 +1,34 @@
+package Basics;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+public class Direct_Locator29_09 {
+	public static void main(String[]args) throws InterruptedException
+	{
+		//-----Locator by using Tag Name-----
+		
+		ChromeDriver driver =  new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+		
+		WebElement searchfield = driver.findElement(By.tagName("input"));
+		searchfield.sendKeys("PS5");
+		Thread.sleep(3000);
+		driver.findElement(By.className("button-1")).click();
+		Thread.sleep(3000);
+		driver.close();
+		
+		
+		//************* For Register Button******************
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("https://demowebshop.tricentis.com/");
+//		Thread.sleep(3000);
+//		driver.findElement(By.className("ico-register")).click();
+//		Thread.sleep(3000);
+//		driver.close();
+	}
+
+}
