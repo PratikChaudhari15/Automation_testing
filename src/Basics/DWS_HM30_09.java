e6d3a34118cdcc8fd64ce7e5dc4a9b614c592148
Pratik Chaudhari <pratik.chaudhari15.03@gmail.com>
2025-10-01 14:36:46 +0530

    DWS Resigter page_HM30_09

---
+++ src/Basics/DWS_HM30_09.java
@@ -0,0 +1,39 @@
+package Basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DWS_HM30_09 {
+	public static void main(String[]args) throws InterruptedException
+	{
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.get("https://demowebshop.tricentis.com/login");
+		Thread.sleep(3000);
+		
+		driver.findElement(By.className("ico-register")).click();
+		Thread.sleep(3000);
+		
+		driver.findElement(By.id("gender-male")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.id("FirstName")).sendKeys("Pratik");
+		Thread.sleep(1000);
+		driver.findElement(By.id("LastName")).sendKeys("Chaudhari");
+		Thread.sleep(1000);
+		driver.findElement(By.id("Email")).sendKeys("pratik.chaudhari1406@gmail.com");
+		Thread.sleep(1000);
+		driver.findElement(By.name("Password")).sendKeys("Pratik@1235");
+		Thread.sleep(1000);
+		driver.findElement(By.id("ConfirmPassword")).sendKeys("Pratik@123");
+		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.id("register-button")).click();
+		
+		Thread.sleep(3000);
+		driver.close();
+		
+	}
+
+}
